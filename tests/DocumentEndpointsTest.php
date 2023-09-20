9c7881147c9d5ec1522ebf092392146fb2fbe118
Nico Orfanos <nico@sigmie.com>
2023-09-20 15:40:35 +0300

    wip

--- tests/DocumentEndpointsTest.php
+++ tests/DocumentEndpointsTest.php
@@ -50,4 +50,91 @@ class DocumentEndpointsTest extends TestCase
 
         $this->assertEquals('route/not_found', $res->json('error'));
     }
+
+    /**
+     * @test
+     */
+    public function get_doc()
+    {
+        $client = new Client(
+            applicationId: getenv('SIGMIE_APPLICATION_ID'),
+            apiKey: getenv('SIGMIE_API_KEY')
+        );
+
+        $index = uniqid();
+
+        $res = $client->upsertDocument($index, [
+            'name' => 'Snowflake'
+        ]);
+
+        $this->assertEquals(201, $res->code());
+
+        $res = $client->getDocument($index, $res->json('_id'));
+
+        $this->assertEquals('Snowflake', $res->json('name'));
+        $this->assertArrayHasKey('autocomplete', $res->json());
+
+        $client->deleteIndex($index);
+    }
+
+    /**
+     * @test
+     */
+    public function patch_document()
+    {
+        $client = new Client(
+            applicationId: getenv('SIGMIE_APPLICATION_ID'),
+            apiKey: getenv('SIGMIE_API_KEY')
+        );
+
+        $index = uniqid();
+
+        $res = $client->upsertDocument($index, [
+            'name' => 'Snowflake'
+        ]);
+
+        $this->assertEquals('Snowflake', $res->json('name'));
+
+        $res = $client->patchDocument($index, [
+            'name' => 'Magic Fire'
+        ], $res->json('_id'));
+
+        $this->assertEquals('Magic Fire', $res->json('name'));
+
+        $this->assertEquals(200, $res->code());
+
+        $client->deleteIndex($index);
+    }
+
+    /**
+     * @test
+     */
+    public function delete_document()
+    {
+        $client = new Client(
+            applicationId: getenv('SIGMIE_APPLICATION_ID'),
+            apiKey: getenv('SIGMIE_API_KEY')
+        );
+
+        $index = uniqid();
+
+        $res = $client->upsertDocument($index, [
+            'name' => 'Snowflake'
+        ]);
+
+        $this->assertEquals(201, $res->code());
+
+        $id = $res->json('_id');
+
+        $res = $client->deleteDocument($index, $id);
+
+        $this->assertEquals($id, $res->json('_id'));
+
+        $res = $client->getDocument($index, $id);
+
+        $this->assertEquals(404, $res->code());
+        $this->assertEquals('document/not_found', $res->json('error'));
+
+        $client->deleteIndex($index);
+    }
 }
